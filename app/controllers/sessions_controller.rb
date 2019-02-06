ea1dc8cc3459b54cb151a200e9ac5fb713a58c84
Zachary Lehmann <zlehmann@gmail.com>
2019-02-06 18:00:17 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
   def destroy
       if !session[:name].nil?
         session[:name]
-      else 
+      else
         session[:name] = nil
       end
   end
