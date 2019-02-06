3904a3ba63611afb278db4979b33787da54d5288
Zachary Lehmann <zlehmann@gmail.com>
2019-02-06 17:51:12 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:name] = params[:name]
-    redirect_to '/'
+    if params[:name] == nil || params[:name] == ""
+      redirect_to '/'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
   end
 
   def require_login
